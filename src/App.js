c2855fb74ad8c6c2f3caae66c0f9e0c32e7469c6
salizarana <salizarana@gmail.com>
2023-11-27 16:13:25 +0545

    Add css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=79542c63a5a2c2777d88607f2331829a`;
+
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Kathmandu</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">Clouds</div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">65°F</p>
+            <p>Feels like</p>
+          </div>
+          <div className="humidity">
+            <p className="bold">20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p className="bold">12MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
